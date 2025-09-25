99f005080c7c89d48d7a8e0e5043617d6347962d
jacob r sayers <82225559+Jacob05-me@users.noreply.github.com>
2025-09-24 22:32:40 -0500

    added to Blog.js and Blog/index.html

--- Ref/Js/Blog.js
+++ Ref/Js/Blog.js
@@ -1,29 +1,89 @@
 
+function WriteComment(form){
+    var formData = new FormData(form)
+    // output as an object
+    console.log(Object.fromEntries(formData));
 
-function WriteComment(none){
+    // ...or iterate through the name-value pairs
+    for (var pair of formData.entries()) {
+        console.log(pair[0] + ": " + pair[1]);
+    }
     const targetElement = document.getElementById("comments-list");
-    const formResults = document.getElementById("comment-form");
-    var obj = {table : []};
-    obj.table.push();
-    const Display = '';
-    targetElement.insertAdjacentHTML("afterbegin", Display)
-}
-function loadPastComments() {
-    console.log("loadPastComments running")
+
+    var OldID = jsonComments.Data.LAST-ID
+    var NewID = OldID + 1;
+    var Name = formData["comment-author"];
+    var NowDate = new Date(month, day, year, hours, minutes, seconds);
+    var Text = formData["comment-text"];
+
+    var obj = {};
+    obj.push({"ID" : NewID, "User" : Name, "Timecode": NowDate, "Text":Text, "Replys":[]});
+    var json = JSON.stringify(obj);
+    var fs = require('fs')
+    
     fetch('/OnlyMats/Ref/json/Comments.json')
         .then(res => res.json())
         .then(data => {
             console.log(data);
-            jsonComments = JSON.parse(data);
-            const targetElement = document.getElementById("comments-list");
-            for(comment in jsonComments.Comments){
-                let replys = ''
-                for(reply in comment.Replys){
-                    replys = replys + '<br><div id="MainCommentContainer"><div id="commentContainer"><div id="CommentInfoContainer"><div id="CommentName">'+reply.User+'</div><div id="CommentTime">'+reply.Timecode+'</div></div><br><hr><div id="CommentContentContainer"><div id="CommentContents">'+reply.Text+'</div></div><div id="CommentIDContainer"><div id="CommentID">ID : '+reply.ID+'</div></div><hr>'
-                }
-                let Display = '<div id="MainCommentContainer"><div id="commentContainer"><div id="CommentInfoContainer"><div id="CommentName">'+comment.User+'</div><div id="CommentTime">'+comment.Timecode+'</div></div><br><hr><div id="CommentContentContainer"><div id="CommentContents">'+comment.Text+'</div></div><div id="CommentIDContainer"><div id="CommentID">ID : '+comment.ID+'</div></div><hr><div id="CommentReplyButtonContainer"><form id="reply-form"><div>Reply?</div><label for="reply-author">Name:</label><input type="text" id="reply-author" name="author" required><label for="reply-text">Comment:</label><textarea id="reply-text" name="comment" rows="1" required></textarea><button onclick="Writereply()" type="submit">Reply</button></form></div><hr><div>replys:</div><div id="reply-container">'+replys+'</div></div></div>'
-                targetElement.insertAdjacentHTML("afterbegin", Display)
-            }
+            const jsonComments = JSON.parse(data);
+            jsonComments.Data = json;
+            fs.writeFile('/OnlyMats/Ref/json/Comments.json', jsonComments, 'utf-8', callback);
         })
-    console.log("loadPastComments ran")
+
+    const Display = '';
+    targetElement.insertAdjacentHTML("afterbegin", Display)
 }
+
+function parseObject(){
+        var obj = {
+            "Comments": [
+                {
+                    "ID": 1,
+                    "User": "Jacob",
+                    "Timecode": "1/12/25 1:12pm",
+                    "Text": "This is a a test comment. [1]",
+                    "Replys": [
+                        {
+                            "ID": 3,
+                            "User": "Jacob",
+                            "Timecode": "1/15/25 12:00pm",
+                            "Text": "This is a a test comment. [1]"
+                        }
+                    ]
+                },
+                {
+                    "ID": 2,
+                    "User": "Jacob",
+                    "Timecode": "1/12/25 1:50pm",
+                    "Text": "This is a a test comment. [2]",
+                    "Replys": []
+                }
+            ]
+        }
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            var item = obj[key];
+            if(typeof item == 'string'){
+                console.log(key + ' -> ' + obj[key]);
+            }
+            else if(typeof item == 'object'){
+                console.log(key + ' -> ' + obj[key]);
+                var x = JSON.stringify(obj[key])
+                var y = JSON.parse(x)
+                console.log(key + ' -> ' + x);
+                console.log(key + ' -> ' + y.ID);
+            }
+            else{
+                console.log(key + ' -> ');
+                parseObject(item);
+            }
+        }
+    }
+}
+
+// // onload = loadPastComments();
+// var x = document.getElementById("comment-form");
+// x.addEventListener("submit", function (e){
+//     e.preventDefault();
+//     WriteComment(e.target)
+// })
